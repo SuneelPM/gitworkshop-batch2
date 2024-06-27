ce70d683e2144ed3b9afdba10b8d810dd2fd23fb
manoj791 <jangammanojkumar16@gmail.com>
2024-06-27 14:59:30 +0530

    manoj

---
+++ manoj1.py
@@ -0,0 +1,11 @@
+def prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for number in range(1, 101):
+    if prime(number):
+        print(number, end=' ')
