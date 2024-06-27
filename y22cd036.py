4aa7b512cad00a263e2fc000ebf289a9b2ac0097
gogulasaimounika <saimounikagogula@gmail.com>
2024-06-27 15:48:04 +0530

    Create y22cd036.py

---
+++ y22cd036.py
@@ -0,0 +1,8 @@
+def primeN():
+    for i in range (2, 101):
+              for j in range (2, 101):
+                    if (i%j==0):
+                       break
+              if i==j:
+                   print(i)
+primeN()
